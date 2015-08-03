f1a45b0c2716dc5efe20f1682710f9d594ed15d5
JanneLindberg <janne-lindberg@bahnhof.se>
2015-08-03 21:57:24 +0200

    Updated dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
-                 [ring "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [compojure "1.3.4"]
+
+;;                 [ring/ring-jetty-adapter "1.3.1"]
+
                  [org.clojure/data.json "0.2.5"]
-                 [ring/ring-json "0.2.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-json "0.4.0"]
                  ]
   :profiles {:uberjar {:main node_monitor.core, :aot :all}}
 
